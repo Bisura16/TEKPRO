437da1facbde46c395407214971ca1b176f2d012
Bimo Surya Anggara <anggarabimo16@gmail.com>
2026-04-01 16:24:54 +0700

    first commit

---
+++ nomor1/Employee.java
@@ -0,0 +1,25 @@
+package nomor1;
+
+// Class Employee
+class Employee {
+    private String name;
+    private int salary;
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + salary;
+    }
+}
---
+++ nomor3/ProblemThree.java
@@ -0,0 +1,19 @@
+package nomor3;
+
+import java.util.Arrays;
+
+public class ProblemThree {
+    public static void main(String[] args) {
+        int arr[] = {12, 4, 3, 1, 9, 657};
+        int n = 3; // Mencari elemen terbesar ke-3
+
+        int ans = Arrays.stream(arr)
+                .boxed() // [2] Konversi ke Stream<Integer>
+                .sorted((a, b) -> Integer.compare(b, a)) // descending
+                .skip(n - 1) // [3] Lewati elemen sebelumnya
+                .findFirst() // [4] Ambil elemen berikutnya
+                .orElse(0); // [5] default jika kosong
+
+        System.out.println("Elemen terbesar ke-3 adalah: " + ans);
+    }
+}
